073de91b79995f01f7f5ce0dbedbc8bd9c6ae00e
firmanfird <firmanfird23@gmail.com>
2025-03-16 21:39:15 +0700

    begin section 5 end 160

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,13 +1,54 @@
+"use client";
 
 import NewsList from '@/app/components/news-list'
-import { DUMMY_NEWS } from '@/dummy-news'
+import { useEffect, useState } from 'react';
+
 
 
 const NewsPage = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+
+  useEffect(() => {
+    async function fetchNews(){
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/news')
+
+      if (!response.ok){
+        setError('Mana Beritanyaaaaa');
+        setIsLoading(false);
+      }
+
+      const news =  await response.json();
+
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />
+  }
+
+
   return (
     <>
         <h1>News Page</h1>
-        <NewsList news={DUMMY_NEWS} />
+        {newsContent}
     </>
     
   )
